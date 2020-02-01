15c1cd14567b82efa4ae5eb3e6e1cd5dafaa3630
pavan097 <pavan970535@gmail.com>
2020-02-01 21:52:20 +0530

    added sections

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog.views import blogData, blogHome, displayContent, getData
 
 urlpatterns=[
     path('create_blog/', blogData, name='blogData'),
-    path('blog_home/', blogHome, name='blogHome'),
+    path('', blogHome, name='blogHome'),
     path('display_content/<int:slug>/', displayContent, name='display_content'),
     path('getData/', getData, name='getData')
 ]
